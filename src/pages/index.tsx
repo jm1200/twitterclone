3d99612de03dcd91a2879bcd38b1691a2854d8da
John McNeill <jh_mcneill@yahoo.ca>
2023-03-29 14:47:50 -0400

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
 
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -20,6 +19,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn && <SignInButton />}</div>
         <div>{!!user.isSignedIn && <SignOutButton />}</div>
+        <div className="">
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
