d6290ab6ddfa8a4247880f67d0cbe99665123c07
John McNeill <jh_mcneill@yahoo.ca>
2023-03-31 11:06:56 -0400

    profile page

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,67 @@
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import type { NextPage, GetStaticProps } from "next";
+import Head from "next/head";
+import { appRouter } from "../server/api/root";
+import { api } from "../utils/api";
+
+import superjson from "superjson";
+import { prisma } from "~/server/db";
+import { PageLayout } from "../components/layout";
+import Image from "next/image";
+
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
+  });
+
+  if (!data) return <div>404</div>;
+  return (
+    <>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <PageLayout>
+        <div className="relative h-36  bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt="asdfasdf"
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl">{`@${data.username ?? ""}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson,
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("No slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsername.prefetch({ username });
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
+
+export default ProfilePage;
