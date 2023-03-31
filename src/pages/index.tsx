cf432b4a1c7f4a71fce487be1ae9cf39b634b8c8
John McNeill <jh_mcneill@yahoo.ca>
2023-03-30 20:29:26 -0400

    toast errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import { api, RouterOutputs } from "~/utils/api";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "../components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -21,6 +22,14 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post. Try again later!");
+      }
     },
   });
 
@@ -41,10 +50,24 @@ const CreatePostWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
       />
-      <button disabled={isPosting} onClick={() => mutate({ content: input })}>
-        Submit
-      </button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Submit</button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center ">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
